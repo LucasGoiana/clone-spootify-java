c7c75f56f8b2432ea6f64ce66417877c6ecc36c4
Lucas Goiana Malicia <lucasgoianam@hotmail.com>
2022-03-06 19:27:12 -0300

    Update Constants.java

--- src/main/java/br/com/clone/spotify/constants/Constants.java
+++ src/main/java/br/com/clone/spotify/constants/Constants.java
@@ -2,7 +2,7 @@ package br.com.clone.spotify.constants;
 
 public class Constants {
 
-    public static final String CLIENT_ID = "524adc28b6c240008e0559e1afe2ca06";
-    public static final String CLIENT_SECRET = "c6767648a9d4447397462e63aad89df4";
+    public static final String CLIENT_ID = "";
+    public static final String CLIENT_SECRET = "";
 
 }
